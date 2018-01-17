da96cd1b55b0c1421a239155ba43e61885cbf766
Patty8909 <patty90_30@hotmail.com>
2018-01-17 08:53:30 -0500

    añade vista home

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     setTimeout(function() { 
-      window.location.href = 'views/principal.html';
+      window.location.href = 'views/home.html';
     }, 11000);
   });
