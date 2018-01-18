026fb474cc6f1cc65d3d20f67e846f696292bd29
lucerohospina <lucerojhp@gmail.com>
2018-01-18 12:36:45 -0500

    agregando funcionalidad de los titulos de peliculas y sus respectivos modales

--- js/movies.js
+++ js/movies.js
@@ -5,34 +5,73 @@ $(document).ready(function() {
     getMovies(searchText);
     event.preventDefault();
   });
+
+  function getMovies(searchText) {
+    console.log(searchText);
+    axios.get('https://www.omdbapi.com/?t=' + searchText + '&apikey=3a181f1c')
+      .then(function(response) {
+        console.log(response);
+        var movies = response;
+        var output = '';
+        $.each(movies, function(index, movie) {
+          if (movie === response.data) {
+            console.log(index + ':' + movie);
+            output += `
+            <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">${movie.Title}</h4>
+          </div>
+          <div class="modal-body text-center">
+            <img class="img-responsive" src="${movie.Poster}">
+          </div>
+          <div class="modal-footer">
+            <p>${movie.Year}
+          </div>
+          `;
+          }
+        });
+        $('.modal-content').html(output);
+      }).catch(function(error) {
+        console.log(error);
+      }); 
+  }
+  
+  $('.open-movie-modal').on('click', movieModal);
+  
+  function movieModal(event) {
+    console.log(event.target);
+    var anchorElement = event.target;
+  
+    console.log(anchorElement.dataset['title']);
+    var movieTitle = anchorElement.dataset['title'];
+  
+    axios.get('http://www.omdbapi.com/?t=' + movieTitle + '&apikey=3a181f1c')
+      .then(function(response) {
+        console.log(response);
+        var movies = response;
+        var output = '';
+        $.each(movies, function(index, movie) {
+          if (movie === response.data) {
+            console.log(index + ':' + movie);
+            output += `
+            <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">${movie.Title}</h4>
+          </div>
+          <div class="modal-body text-center">
+            <img src="${movie.Poster}">
+          </div>
+          <div class="modal-footer">
+            <p>${movie.Year}
+          </div>
+          `;
+          }
+        });
+        $('.modal-content').html(output);
+      }).catch(function(error) {
+        console.log(error);
+      });
+  }
 });
 
-function getMovies(searchText) {
-  console.log(searchText);
-  axios.get('https://www.omdbapi.com/?t=' + searchText + '&apikey=3a181f1c')
-    .then(function(response) {
-      console.log(response);
-      var movies = response;
-      var output = '';
-      $.each(movies, function(index, movie) {
-        if (movie === response.data) {
-          console.log(index + ':' + movie);
-          output += `
-          <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title">${movie.Title}</h4>
-        </div>
-        <div class="modal-body text-center">
-          <img class="img-responsive" src="${movie.Poster}">
-        </div>
-        <div class="modal-footer">
-          <p>${movie.Year}
-        </div>
-        `;
-        }
-      });
-      $('.modal-content').html(output);
-    }).catch(function(error) {
-      console.log(error);
-    }); 
-}
+
